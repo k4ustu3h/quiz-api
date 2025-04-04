d162f2ed43970777fda26d33adcfc74140a7cf8b
Kaustubh Ladiya <k4ustu3h@gmail.com>
2025-04-05 03:15:08 +0530

    Update MongoDB connection URL

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const PORT = 9090;
 const quizRoutes = require("./routes/quizRoutes");
 app.use("/api/questions", quizRoutes);
 
-
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/quizDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB Connected"))
+	.catch((err) => console.error("MongoDB connection error:", err));
 
 // Start Server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+	console.log(`Server running on http://localhost:${PORT}`)
+);
